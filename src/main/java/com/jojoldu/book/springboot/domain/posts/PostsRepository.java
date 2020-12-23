5ab02ecebab332380c1f17ead824ed7e3279138c
ljh95 <ilegwmam@gmail.com>
2020-12-23 10:30:55 +0900

    글 삭제 완료

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
 /*qhxhd ibatis등에서 Dao라고 불리는 DB Layer 접근자입니다. JPA에선 Repository라고 부르며 인터페이스로 생성합니다. 단순히 인터헤이스를 생성 후, JpaRepository<Entity 클래스, PK타입>
 * 을 상속하면 기본적인 CRUD메소드가 자동으로 생성됩니다.
